8672bc8796008c9e0d62d5f85540928eb0c19bca
KingTayko <thaigopjs123@gmail.com>
2023-11-09 00:50:04 -0300

    Herenca

---
+++ Herenca/Herenca01/src/Herenca/Gerente.java
@@ -0,0 +1,11 @@
+package Herenca;
+
+public class Gerente extends Funcionario {
+	
+	public void fecharCaixa() {
+		System.out.println("Fechando caixa...");
+	}
+
+
+}
+
---
+++ Herenca/Herenca01/src/Herenca/Principal.java
@@ -0,0 +1,40 @@
+package Herenca;
+
+public class Principal {
+	public static void main(String[] arg) {
+	Funcionario f1 = new Funcionario();
+	
+	f1.setNome("Gustavo");
+	f1.setIdade(19);
+	f1.setSexo("M");
+	System.out.println("Funcionario " + f1.toString());
+	f1.baterPonto();
+	
+	System.out.println("\n");
+	Gerente g1 = new Gerente();
+	g1.setNome("Thiago");
+	g1.setIdade(18);
+	g1.setSexo("M");
+	System.out.println("Gerente " + g1.toString());
+	g1.fecharCaixa();
+	
+	System.out.println("\n");
+	Vendedor v1 = new Vendedor();
+	v1.setNome("Fernanda");
+	v1.setIdade(18);
+	v1.setSexo("F");
+	System.out.println("Vendedor(a) " + v1.toString());
+	v1.realizaVenda();
+	
+	System.out.println("\n");
+	Faxineiro f2 = new Faxineiro();
+	f2.setNome("Marcos");
+	f2.setIdade(32);
+	f2.setSexo("M");
+	System.out.println("Faxineiro " + f2.toString());
+	f2.solicitarMaterial();
+			
+	
+			
+	}
+}
